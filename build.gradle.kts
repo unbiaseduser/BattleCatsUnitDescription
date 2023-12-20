18708b7c665eb7666de6bf78688587f84bcf6688
bruh <quangtrung02hn16@gmail.com>
2023-12-20 20:48:00 +0700

    Remove unneeded suppress

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
     id("io.freefair.lombok") version "6.6.1"
     alias(libs.plugins.room) apply false
 }
-true // Needed to make the Suppress annotation work for the plugins block
 
 buildscript {
     dependencies {
